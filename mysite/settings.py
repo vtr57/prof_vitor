2f571dd90d0d09f4a5e8b5faab579a851e766b06
vtr57 <professorvitorfisica@gmail.com>
2024-07-26 10:18:47 -0300

    criando app blog.

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'my_app/static'),
+    os.path.join(BASE_DIR, 'blog/static'),
 ]
 
 # Default primary key field type
